361bc488fb448237345c6f843d79603c44531424
Simardeep <skhinda7@gmail.com>
2023-04-26 00:27:01 -0400

    Developing quote button

--- javascript/function.js
+++ javascript/function.js
@@ -1,10 +1,16 @@
-function loadTextFromFile(file, headingId) {
-    fetch(file)
-        .then(response => response.text())
-        .then(text => {
-            document.getElementById(headingId).textContent = text;
-        })
-        .catch(error => {
-            console.error(`Error loading text from file: ${error}`);
-        });
+async function getRandQuote() {
+    const api_url = 'https://api.quotable.io/random'
+    const test = ('../javascript/test.json')
+    const response = await fetch(api_url, {
+        method: 'GET',
+    });
+    const result = await response.json();
+    const myQuote = document.getElementById('generated-quote');
+    const myAuthor = document.getElementById('author')
+    const quote = result.content;
+    const author = result.author;
+    myQuote.textContent = (`"${quote}"`);
+    myAuthor.textContent = ("―" + author);
 }
+
+window.addEventListener('load', getRandQuote());
