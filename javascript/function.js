e12a7ffc98dd6266d522af26a35018b82d257ced
Simardeep <skhinda7@gmail.com>
2023-05-24 22:35:29 -0400

    Added projects

--- javascript/function.js
+++ javascript/function.js
@@ -25,28 +25,34 @@ async function fillTextToDiv(text, htmlLocation) {
     textDiv.textContent = result;
 }
 
-function pickRandGradient() {
-    const list = fetchJSON('../javascript/randomgrad.json');
+let slideIndex = 1;
+window.addEventListener('load', function () {
+    showSlides(slideIndex);
+});
 
-    var randomIndex = Math.floor(Math.random() * list.length);
-    var randomName = list[randomIndex].name;
-    var colors = [list[randomIndex].colors[0], list[randomIndex].colors[1], list[randomIndex].colors[2]];
-
-    console.log("Random Name: " + randomName);
-    console.log("Colors: " + colors);
-
-    const pickedColor = [randomName, colors];
-
-    return pickedColor;
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
 }
 
-async function fetchJSON(location) {
-    const object = await fetch(location, {
-        method: 'GET',
-    })
-    return object.json();
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
 }
 
-pickRandGradient();
-
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("project");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
 
