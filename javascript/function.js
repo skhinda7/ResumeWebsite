d976ec9fb312a9a9068389b43107879ce116127f
Simardeep <skhinda7@gmail.com>
2023-07-04 21:08:25 -0500

    Reconstruction in progress

--- javascript/function.js
+++ javascript/function.js
@@ -56,15 +56,14 @@ function showSlides(n) {
     dots[slideIndex - 1].className += " active";
 }
 
-window.onload = function() {
+window.onload = function () {
     const menu_btn = document.querySelector('.hamburger');
     const mobile_menu = document.querySelector('.mobile-nav');
 
-    menu_btn.addEventListener('click', function() {
+    menu_btn.addEventListener('click', function () {
         menu_btn.classList.toggle("is-active");
         mobile_menu.classList.toggle("is-active");
     })
 
-    
-}
 
+}
